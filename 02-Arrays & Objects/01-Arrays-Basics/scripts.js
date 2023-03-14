362f00c23e3d3642c2873ad8a58a54951e12077a
Web-Dev-Robi <cordiscobrian@gmail.com>
2023-03-14 19:21:23 +0100

    arrays

---
+++ 02-Arrays & Objects/01-Arrays-Basics/scripts.js
@@ -0,0 +1,26 @@
+let x;
+
+// Array Literal
+
+const numbers = [12, 34, 65, 77, 43, 55, 666, 23454];
+
+// Array Constructor
+
+const fruits = new Array('Apples', 'Pear', 'Grape', 'Orange');
+
+x = numbers[0];
+
+console.log(fruits)
+
+// Adding numbers with bracket notation
+
+x = numbers[0] + numbers[3];
+
+
+// Object literal
+
+x = `My favorite fruit is a ${fruits[2]}`
+
+x = numbers.length
+
+console.log(x);
