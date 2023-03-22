7a004030aed5ff4f8c55146ca35e225eaf785909
Web-Dev-Robi <cordiscobrian@gmail.com>
2023-03-22 14:31:52 +0100

    DOM modules done

--- 06-The DOM/14-adding-removing-styles-classes/script.js
+++ 06-The DOM/14-adding-removing-styles-classes/script.js
@@ -1,2 +1,20 @@
+// Adding Removing Styles & Classes
 
+const text = document.querySelector('p')
+const itemList = document.querySelector('.item-list')
+const items = itemList.querySelectorAll('li');
 
+function run() {
+
+    console.log(itemList.classList)
+
+    itemList.classList.forEach((c) => console.log(c))
+    
+    items.forEach((item, index) => {
+        item.style.color = 'red'
+    })
+
+}
+
+// Click event onclick
+document.querySelector('button').onclick = run;
