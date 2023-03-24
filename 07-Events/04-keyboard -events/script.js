5439199ccbb24941a2db038aff3feb54e0c73f51
Web-Dev-Robi <cordiscobrian@gmail.com>
2023-03-24 11:59:21 +0100

    DOM Challange Complete

--- 07-Events/04-keyboard -events/script.js
+++ 07-Events/04-keyboard -events/script.js
@@ -1,17 +1,24 @@
 const itemInput = document.getElementById('item-input');
 
-
+// registers just the key press once
 const onKeyPress = e => {
     console.log('keypress');
 };
 
-
+// registers when key press is released
 const onKeyUp = e => {
     console.log('keyup');
 };
 
+// registers the key press even if held down
 const onKeyDown = e => {
-    console.log('keydown');
+    // Key
+    console.log(e.key);
+    // KeyCode
+    console.log(e.keyCode);
+
+    // code
+    console.log(e.code);
 };
 
 
--- 07-Events/05-event-keycode-project/script.js
+++ 07-Events/05-event-keycode-project/script.js
@@ -1,10 +1,63 @@
-const keyPress = document.querySelector('#insert')
+// Method 1
+// window.addEventListener('keydown', (e) => {
+//   const insert = document.getElementById('insert');
+
+//   insert.innerHTML = `
+//     <div class="key">
+//       ${e.key === ' ' ? 'Space' : e.key}
+//       <small>e.key</small>
+//     </div>
+
+//     <div class="key">
+//       ${e.keyCode}
+//       <small>e.keyCode</small>
+//     </div>
+
+//     <div class="key">
+//       ${e.code}
+//       <small>e.code</small>
+//     </div>
+//   `;
+// });
+
+// Method 2
+function showKeyCodes(e) {
+    // Grabbing the ID
+    const insert = document.getElementById('insert');
+    // Set the innerHTML to empty string or object 
+    insert.innerHTML = '';
 
 
-function showKeysCodes(e) {
-    console.log(e.key.value)
-    console.log(e.keyCode.value)
-    console.log(e.code.value)
+    // Creating an Object for data to display in newly created elements
+    const keyCodes = {
+        'e.key': e.key === ' ' ? 'Space' : e.key,
+        'e.keyCode': e.keyCode,
+        'e.code': e.code,
+    };
+  
+    // for loop 
+    for (let key in keyCodes) {
+        // Element to be created
+        const div = document.createElement('div');
+        // Defining class
+        div.className = 'key';
+        // creating small element tag
+        const small = document.createElement('small');
+        // node to be created with key code values
+        const keyText = document.createTextNode(key);
+        // creating a node with values from keyText
+        const valueText = document.createTextNode(keyCodes[key]);
+        // must append to create the element we want to display
+        small.appendChild(keyText);
+
+        div.appendChild(valueText);
+        div.appendChild(small);
+
+        insert.appendChild(div);
+    }
 }
 
-keyPress.addEventListener('onkeydown', showKeysCodes)
+
+// window used since the keys will be pressed anywhere on the screen
+
+window.addEventListener('keydown', showKeyCodes);
